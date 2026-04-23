64a06376a941d8307d5ce39f7464bd0fc80ad13a
nickygrey <sgp5vb2qjd@privaterelay.appleid.com>
2026-04-23 23:35:01 +0000

    Fix calculator imports

--- python-import_modules/100-my_calculator.py
+++ python-import_modules/100-my_calculator.py
@@ -1,15 +1,15 @@
 #!/usr/bin/python3
 from calculator_1 import add, sub, mul, div
-import sys
+from sys import argv, exit
 
 if __name__ == "__main__":
-    if len(sys.argv) != 4:
+    if len(argv) != 4:
         print("Usage: ./100-my_calculator.py <a> <operator> <b>")
-        sys.exit(1)
+        exit(1)
 
-    a = int(sys.argv[1])
-    b = int(sys.argv[3])
-    operator = sys.argv[2]
+    a = int(argv[1])
+    b = int(argv[3])
+    operator = argv[2]
 
     if operator == "+":
         result = add(a, b)
@@ -21,6 +21,6 @@ if __name__ == "__main__":
         result = div(a, b)
     else:
         print("Unknown operator. Available operators: +, -, * and /")
-        sys.exit(1)
+        exit(1)
 
     print("{} {} {} = {}".format(a, operator, b, result))
