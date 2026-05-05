278e2a78467b1e17dce25f10019a7033b344556e
nickygrey <sgp5vb2qjd@privaterelay.appleid.com>
2026-05-05 23:06:45 +0000

    Add size validation to Square

---
+++ python-classes/2-square.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""Square module."""
+
+
+class Square:
+    """Square class."""
+
+    def __init__(self, size=0):
+        """Create square."""
+        if type(size) is not int:
+            raise TypeError("size must be an integer")
+
+        if size < 0:
+            raise ValueError("size must be >= 0")
+
+        self.__size = size
