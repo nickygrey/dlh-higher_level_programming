87486f3d6a9e7587b7ad360eaefb349b8bc17d54
nickygrey <sgp5vb2qjd@privaterelay.appleid.com>
2026-05-10 15:50:40 +0000

    Add pickle serialization class

---
+++ python-serialization/task_01_pickle.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python3
+"""Module for pickling custom objects."""
+
+import pickle
+
+
+class CustomObject:
+    """Class that defines a custom object."""
+
+    def __init__(self, name, age, is_student):
+        """Create a custom object."""
+        self.name = name
+        self.age = age
+        self.is_student = is_student
+
+    def display(self):
+        """Display object attributes."""
+        print("Name: {}".format(self.name))
+        print("Age: {}".format(self.age))
+        print("Is Student: {}".format(self.is_student))
+
+    def serialize(self, filename):
+        """Serialize object and save it to a file."""
+        with open(filename, "wb") as f:
+            pickle.dump(self, f)
+
+    @classmethod
+    def deserialize(cls, filename):
+        """Deserialize object from a file."""
+        try:
+            with open(filename, "rb") as f:
+                return pickle.load(f)
+        except Exception:
+            return None
