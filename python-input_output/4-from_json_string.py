c1d7f0b746c8205cf0c92a2c27271e1aa81a8642
nickygrey <sgp5vb2qjd@privaterelay.appleid.com>
2026-05-10 14:40:06 +0000

    Add JSON string parsing

---
+++ python-input_output/4-from_json_string.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""Module that converts JSON string to object."""
+
+import json
+
+
+def from_json_string(my_str):
+    """Return Python object from JSON string."""
+    return json.loads(my_str)
